3759b66f23be8257c19523c47ff46ae8037a25d3
PSkrabneu <skrebnev@gmail.com>
2024-07-15 13:46:45 +0200

    Implemented Parameterized test

--- src/test/java/com/healthycodder/app/BMICalculatorTest.java
+++ src/test/java/com/healthycodder/app/BMICalculatorTest.java
@@ -10,6 +10,9 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BMICalculatorTest {
 
@@ -34,11 +37,27 @@ public class BMICalculatorTest {
       + "Let's create an interface with the methods that represent scenarios that"
       + " we will be automating:";
 
-  @Test
-  public void testShouldTrueInCaseDietRecommended() {
+  @ParameterizedTest
+  @ValueSource(doubles = {89.0, 95.0, 110.0})
+  public void testShouldTrueInCaseDietRecommended(Double coderWeight) {
     // given
-    double weight = 89.0;
+    double weight = coderWeight;
     double height = 1.72;
+
+    // when
+    boolean isRecommended = BMICalculator.isDietRecommended(weight, height);
+
+    // then
+    Assertions.assertTrue(isRecommended);
+  }
+
+  @ParameterizedTest(name = "weight={0}, height={1}")
+  @CsvSource(value = {"89.0, 1.72", "95.0, 1.75", "110.0, 1.78"})
+  public void testShouldTrueInCaseDietRecommendedParameterized(Double coderWeight
+  , Double coderHeight) {
+    // given
+    double weight = coderWeight;
+    double height = coderHeight;
 
     // when
     boolean isRecommended = BMICalculator.isDietRecommended(weight, height);
